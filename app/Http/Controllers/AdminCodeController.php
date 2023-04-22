17dd995d19afb008bbd87de94e92d8cc5265d865
Daniel@LAPTOP-GD1ASI6D <hosstin_rusel@hotmail.com>
2023-04-22 16:57:45 -0600

    Fix Pusher

--- app/Http/Controllers/AdminCodeController.php
+++ app/Http/Controllers/AdminCodeController.php
@@ -115,11 +115,11 @@ class AdminCodeController extends Controller
         /* $qr->activo = $newStatus;
         $qr->save(); */
         $pusher = new Pusher(
-            env('PUSHER_APP_KEY'),
-            env('PUSHER_APP_SECRET'),
-            env('PUSHER_APP_ID'),
+            env('2b67b15a01669d19aaba'),
+            env('b51fde818d893e708dc6'),
+            env('1586700'),
             [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => env('us2'),
                 'useTLS' => true,
             ]
         );
